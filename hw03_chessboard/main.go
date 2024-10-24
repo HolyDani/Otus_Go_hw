3be10c71192cbd7f4fdcae17418d361f0af7de10
Daniel Vladimirov <powerofholydani@gmail.com>
2024-10-24 11:46:31 +0300

    HW Done! Created chessboard with custom size.

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,27 @@
 package main
 
+import "fmt"
+
 func main() {
-	// Place your code here.
+	var size int
+	var board string
+	fmt.Println("Enter size of board: ")
+	fmt.Scan(&size)
+	if size == 0 {
+		fmt.Println("You enter zero value")
+		return
+	}
+
+	for i := 0; i < size; i++ {
+		for j := 0; j < size; j++ {
+			if (i+j)%2 == 0 {
+				board += "#"
+			} else {
+				board += " "
+			}
+		}
+		board += "\n"
+	}
+
+	fmt.Println(board)
 }

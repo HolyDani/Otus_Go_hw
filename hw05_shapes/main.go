4784ffdca2058e5fa109699535539d863757d690
Daniel Vladimirov <powerofholydani@gmail.com>
2024-11-07 14:38:05 +0300

    Changed error statement

--- hw05_shapes/main.go
+++ hw05_shapes/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 
 	shapes "github.com/Otus_hw/HolyDani/hw05_shapes/interface"
@@ -14,30 +15,30 @@ func main() {
 	rectangle1 := rectangle.NewRectangle(10, 5)
 	triangle1 := triangle.NewTriangle(8, 6)
 	res, err := calculateArea(circle1)
-	if err == "" {
-		fmt.Printf("Круг: радиус %v\nПлощадь: %v\n\n", circle1.Radius(), res)
-	} else {
+	if err != nil {
 		fmt.Printf("Ошибка: %s\n", err)
+	} else {
+		fmt.Printf("Круг: радиус %v\nПлощадь: %v\n\n", circle1.Radius(), res)
 	}
 
 	res, err = calculateArea(rectangle1)
-	if err == "" {
-		fmt.Printf("Прямоугольник: ширина %v, высота %v\nПлощадь: %v\n\n", rectangle1.Width(), rectangle1.Height(), res)
-	} else {
+	if err != nil {
 		fmt.Printf("Ошибка: %s\n", err)
+	} else {
+		fmt.Printf("Прямоугольник: ширина %v, высота %v\nПлощадь: %v\n\n", rectangle1.Width(), rectangle1.Height(), res)
 	}
 
 	res, err = calculateArea(triangle1)
-	if err == "" {
-		fmt.Printf("Треугольник: основание %v, высота %v\nПлощадь: %v\n\n", triangle1.Base(), triangle1.Height(), res)
-	} else {
+	if err != nil {
 		fmt.Printf("Ошибка: %s\n", err)
+	} else {
+		fmt.Printf("Треугольник: основание %v, высота %v\nПлощадь: %v\n\n", triangle1.Base(), triangle1.Height(), res)
 	}
 }
 
-func calculateArea(s shapes.Shape) (float64, string) {
+func calculateArea(s shapes.Shape) (float64, error) {
 	if s == nil {
-		return 0, "Переданный объект не является фигурой"
+		return 0, errors.New("переданный объект не является фигурой")
 	}
-	return s.Area(), ""
+	return s.Area(), nil
 }

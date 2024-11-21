197571371459c76d5b0d77eaf130d24b237d8c10
Daniel Vladimirov <powerofholydani@gmail.com>
2024-11-21 15:02:26 +0300

    HW 7 Done! Created func for separate words and counting numbers for each one

--- hw07_word_counter/main.go
+++ hw07_word_counter/main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+var ErrEmptyString = errors.New("empty string")
+
 func main() {
-	// Place your code here.
+	text := "Hello my friend. You are my best friend!"
+	words, err := countWords(text)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for key, value := range words {
+		fmt.Printf("Word: %s = %d\n", key, value)
+	}
+}
+
+func countWords(str string) (map[string]uint, error) {
+	re := regexp.MustCompile(`[[:punct:]]`)
+	cleanedText := re.ReplaceAllString(str, "")
+	if len(cleanedText) > 0 && strings.TrimSpace(cleanedText) != "" {
+		wordCount := make(map[string]uint)
+		cleanedText = strings.ToLower(cleanedText)
+		words := strings.Fields(cleanedText)
+		for _, word := range words {
+			wordCount[word]++
+		}
+		return wordCount, nil
+	}
+	return nil, fmt.Errorf("%w", ErrEmptyString)
 }

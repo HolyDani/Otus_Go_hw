a72474d5ed14d34d9ba882130d292e5253456c15
Daniel Vladimirov <powerofholydani@gmail.com>
2024-12-09 16:31:50 +0300

    Realised Marshal and Unmarshal methods for JSON, XML, YAML, GOB and MsgPack

--- hw09_serialize/go.mod
+++ hw09_serialize/go.mod
@@ -1,3 +1,16 @@
-module github.com/fixme_my_friend/hw09_serialize
+module github.com/HolyDani/Otus_Go_hw/hw09_serialize
 
-go 1.20
+go 1.21
+
+toolchain go1.23.2
+
+require (
+	github.com/vmihailenco/msgpack v4.0.4+incompatible
+	gopkg.in/yaml.v2 v2.4.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/protobuf v1.35.2 // indirect
+)
--- hw09_serialize/main.go
+++ hw09_serialize/main.go
@@ -1,5 +1,90 @@
 package main
 
-func main() {
-	// Place your code here.
+import (
+	"bytes"
+	"encoding/gob"
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+
+	"github.com/vmihailenco/msgpack"
+	"gopkg.in/yaml.v2"
+)
+
+type Book struct {
+	ID     uint    `json:"id" xml:"id" yaml:"id"`
+	Size   uint    `json:"size" xml:"size" yaml:"size"`
+	Year   uint    `json:"year" xml:"year" yaml:"year"`
+	Rate   float64 `json:"rate" xml:"rate" yaml:"rate"`
+	Title  string  `json:"title" xml:"title" yaml:"title"`
+	Author string  `json:"author" xml:"author" yaml:"author"`
 }
+
+func SerializeJSON(book []Book) ([]byte, error) {
+	return json.Marshal(book)
+}
+
+func DeserializeJSON(data []byte) ([]Book, error) {
+	var books []Book
+	err := json.Unmarshal(data, &books)
+	if err != nil {
+		return nil, fmt.Errorf("deserializeJSON: %w", err)
+	}
+	return books, nil
+}
+
+func SerializeXML(book []Book) ([]byte, error) {
+	return xml.Marshal(book)
+}
+
+func DeserializeXML(data []byte) ([]Book, error) {
+	var books []Book
+	err := xml.Unmarshal(data, &books)
+	if err != nil {
+		return nil, fmt.Errorf("deserializeXML: %w", err)
+	}
+	return books, nil
+}
+
+func SerializeYAML(book []Book) ([]byte, error) {
+	return yaml.Marshal(book)
+}
+
+func DeserializeYAML(data []byte) ([]Book, error) {
+	var books []Book
+	err := yaml.Unmarshal(data, &books)
+	if err != nil {
+		return nil, fmt.Errorf("deserializeYAML: %w", err)
+	}
+	return books, nil
+}
+
+func SerializeGOB(book []Book) ([]byte, error) {
+	var buff bytes.Buffer
+	enc := gob.NewEncoder(&buff)
+	err := enc.Encode(book)
+	return buff.Bytes(), err
+}
+
+func DeserializeGOB(data []byte) ([]Book, error) {
+	var books []Book
+	buff := bytes.NewBuffer(data)
+	dec := gob.NewDecoder(buff)
+	err := dec.Decode(&books)
+	return books, err
+}
+
+func SerializeMsgPack(book []Book) ([]byte, error) {
+	return msgpack.Marshal(book)
+}
+
+func DeserializeMsgPack(data []byte) ([]Book, error) {
+	var books []Book
+	err := msgpack.Unmarshal(data, &books)
+	if err != nil {
+		return nil, fmt.Errorf("deserializeMsgPack: %w", err)
+	}
+	return books, nil
+}
+
+func main() {}
